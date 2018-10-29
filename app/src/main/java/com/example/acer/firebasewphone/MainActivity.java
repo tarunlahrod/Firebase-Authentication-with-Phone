76eda70a69c2b7b1f826e447b78717fd216a3ff5
tarunlahrod <lahrod.tarun46@gmail.com>
2018-10-29 16:11:47 +0530

    First Commit

---
+++ app/src/main/java/com/example/acer/firebasewphone/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.acer.firebasewphone;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextphone, editTextVerificationCode;
+
+    FirebaseAuth mAuth;
+
+    String codeSent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextphone = findViewById(R.id.phone);
+        editTextVerificationCode = findViewById(R.id.codeReceived);
+    }
+
+    public void onClickGetVerification(View view)
+    {
+        sendVerificationCode();
+    }
+
+    public void onClickVerify(View View)
+    {
+        verifySignInCode();
+    }
+
+    private void verifySignInCode(){
+        String code = editTextVerificationCode.getText().toString();
+        if(code.isEmpty()){
+            Toast.makeText(this,"Enter verification code",Toast.LENGTH_SHORT).show();
+            editTextVerificationCode.requestFocus();
+        }
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //here we can open a new activity
+                            Toast.makeText(getApplicationContext(),"Login successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                Toast.makeText(getApplicationContext(),"Incorrect Verification code", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode()
+    {
+        String phoneNumber = editTextphone.getText().toString();
+        if(phoneNumber.isEmpty()){
+            Toast.makeText(this,"Enter phone number",Toast.LENGTH_SHORT).show();
+            editTextphone.requestFocus();
+        }
+
+        if(phoneNumber.length() < 10){
+            Toast.makeText(this,"Enter correct phone number",Toast.LENGTH_SHORT).show();
+            editTextphone.requestFocus();
+        }
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            codeSent = s;
+        }
+    };
+}
